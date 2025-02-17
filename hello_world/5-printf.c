c8806c0f361548359eee14371b756121303ad929
f-qrm <qerimifjolla7@gmail.com>
2025-02-17 09:19:17 +0100

    with proper grammar, but the outcome is a piece of art

---
+++ hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *printf - print with proper grammar, but the outcome is a piece of art,
+ *
+ *return always 0
+ */
+int main(void)
+{
+printf(with proper grammar, but the outcome is a piece of art,);
+return 0;
+}
