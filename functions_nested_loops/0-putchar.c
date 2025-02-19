db0001aff46608427131223bfc0b3833ff92081d
f-qrm <qerimifjolla7@gmail.com>
2025-02-19 15:00:33 +0100

    creation entete

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,3 +1,32 @@
 #include <unistd.h>
+#include "main.h"
 /**
+ *_putchar - writes the character c to stdout
+ * @c: The character to print
  *
+ *Return: succes 1
+ */
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
+int main(void)
+/**
+ *main - entry point
+ *
+ *Return: always 0
+ */
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
