c636605464e4637dae436eb82a861dc94ecb148f
f-qrm <qerimifjolla7@gmail.com>
2025-02-24 19:21:02 +0100

    Write a program that prints the numbers from 1 to 100, followed by a new line

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *Return: always 0
+ */
+int main(void)
+{
+	int nr;
+
+	for (nr = 1; nr <= 100; nr++)
+	{
+		if (nr % 3 == 0 && nr % 5 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else if (nr % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (nr % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else
+		{
+			printf("%d ", nr);
+		}
+	}
+	return (0);
+}
