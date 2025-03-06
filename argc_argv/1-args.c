da7782a334a9d9dd4ca2a28d70cecd4a66f8f146
f-qrm <qerimifjolla7@gmail.com>
2025-03-06 23:42:30 +0100

    Write a program that prints the number of arguments passed into it

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -6,11 +6,10 @@
  *(including the program name).
  *@argv: An array of strings representing the arguments passed to the program.
  *The first element (argv[0]) is the program name itself
- *Return: 0 
- * 
+ *Return: 0
  */
 int main(int argc, char *argv[] __attribute__((unused)))
 {
-	printf("%d\n", argc -1);
-	return(0);
+	printf("%d\n", argc - 1);
+	return (0);
 }
