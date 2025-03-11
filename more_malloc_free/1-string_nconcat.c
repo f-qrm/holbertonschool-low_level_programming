8dd6fade19c3928fd6efa6addc9123f1265e1187
f-qrm <qerimifjolla7@gmail.com>
2025-03-11 12:16:09 +0100

    Write a function that concatenates two strings

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -1,7 +1,5 @@
 #include "main.h"
 #include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
 /**
  * 
  * 
@@ -11,8 +9,6 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int i = 0;
 	unsigned int j = 0;
 	unsigned int f;
-	unsigned int len1;
-	unsigned int len2;
 	char *resultat;
 
 	if (s1 == NULL)
@@ -31,13 +27,19 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		j++;
 	}
-	len1 = strlen(s1);
-	len2 = strlen(s2);
-	if (n >= len2)
+	while (s1[i])
 	{
-		n = len2;
+		i++;
 	}
-	resultat = malloc (len1 + n + 1);
+	while (s2[j])
+	{
+		j++;
+	}
+	if (n > j)
+	{
+		n = j;
+	}
+	resultat = malloc (1 + n + 1);
 	if (resultat == NULL)
 	{
 		return(NULL);
@@ -46,7 +48,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		resultat[f] = s1[f];
 	}
-	for (f = 0; f < j; f++)
+	for (f = 0; f < n; f++)
 	{
 		resultat[i + f] = s2[f];
 	}
