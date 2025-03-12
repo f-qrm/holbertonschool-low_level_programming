cf8cd2d4f532ec4f9e4e093df7f1f90518cd47d8
f-qrm <qerimifjolla7@gmail.com>
2025-03-12 11:11:03 +0100

    Define a new type struct dog with the following elements

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,13 +1,15 @@
 #ifndef DOG_H
 #define DOG_H
 /**
- * 
- * 
+ * struct dog - A structure representing a dog.
+ *@name: A pointer to a string representing the dog's name (char *).
+ *@age: The dog's age (float).
+ *@owner: A pointer to a string representing the dog's owner's name (char *)
  */
 struct dog
 {
-    char *name;
-    float age;
-    char *owner;
+	char *name;
+	float age;
+	char *owner;
 };
 #endif
