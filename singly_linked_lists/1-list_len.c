461d7fcb0515de10997d3656b9d58b2c7a43854c
f-qrm <qerimifjolla7@gmail.com>
2025-03-31 12:21:57 +0200

    add space for betty

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -8,11 +8,11 @@ size_t list_len(const list_t *h)
 {
 	size_t count = 0;
 
-	while(h != 0)
+	while (h != 0)
 	{
-		h= h->next;
+		h = h->next;
 		count++;
 	}
 
-	return(count);
+	return (count);
 }
