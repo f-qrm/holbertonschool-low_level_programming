f147ec4a8fb480d1513d4642e9ac804ccfce22ba
f-qrm <qerimifjolla7@gmail.com>
2025-03-31 17:58:20 +0200

    write a function that frees a list_t list.

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+/**
+ * free_list - Frees a singly linked list
+ *@head: Pointer to the first node of the list to be freed.
+ */
+void free_list(list_t *head)
+{
+	list_t *ptr;
+
+	while(head != NULL)
+	{
+		ptr = head;
+		head = head->next;
+		free (ptr->str);
+		free (ptr);
+	}
+}
