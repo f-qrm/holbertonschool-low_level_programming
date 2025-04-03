2a8e0ec1dc70026950775e288a171f48d14a4ddd
f-qrm <qerimifjolla7@gmail.com>
2025-04-03 13:27:52 +0200

    Add comments for betty

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -3,8 +3,11 @@
 #include <fcntl.h>
 #include <unistd.h>
 /**
- * 
- * 
+ * read_textfile - Reads a text file and prints it to the POSIX standard output
+ *@filename: The name of the file to read
+ *@letters: The number of letters to read and print
+ *Return: The actual number of letters read and printed,
+ *or 0 if an error occurs (e.g., file cannot be opened, read, or written)
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
@@ -27,7 +30,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		close(fd);
 		return (0);
 	}
-	rbytes = read (fd, ltr, letters);
+	rbytes = read(fd, ltr, letters);
 	if (rbytes == -1)
 	{
 		free(ltr);
@@ -39,9 +42,9 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	{
 		free(ltr);
 		close(fd);
-		return(0);
+		return (0);
 	}
 	free(ltr);
 	close(fd);
-	return(wbytes);
+	return (wbytes);
 }
