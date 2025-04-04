434c4c4b2f8265a0a27457f308e4f82aeddd4769
f-qrm <qerimifjolla7@gmail.com>
2025-04-04 14:36:48 +0200

    Write a program that copies the content of a file to another file

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -82,7 +82,7 @@ int copiefile(const char *file_from, char *file_to)
 		exit(98);
 	}
 
-	fd_to = open(file_to, O_RDWR | O_CREAT | O_TRUNC, 0664);
+	fd_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (fd_to == -1)
 	{
 		dprintf(2, "Error: Can't write to %s\n", file_to);
